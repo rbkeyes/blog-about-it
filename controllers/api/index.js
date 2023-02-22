32f064bbb890480a3cf59d4e73040e02bf8068b6
Reed Keyes <rbkeyes@gmail.com>
2023-02-21 23:07:18 -0800

    added file structure, started adding files

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+// const userRoutes = require('./userRoutes');
+// const blogRoutes = require('./blogRoutes');
+// const commentRoutes = require('./blogCommentRoutes')
+// const blogCommentRoutes = require('./blogCommentRoutes')
+
+// router.use('/users', userRoutes);
+// router.use('/blog', blogRoutes);
+// router.use('/comment', commentRoutes);
+// router.use('/blog-comments', blogCommentRoutes);
+
+module.exports = router;
