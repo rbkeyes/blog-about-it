859d30af516f5ba2e24a931f1a7af1a0b1a342ef
Reed Keyes <rbkeyes@gmail.com>
2023-02-21 23:19:25 -0800

    render login page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,4 +26,14 @@ router.get('/', async (req, res) => {
     }
   });
 
+  router.get('/login', async (req, res) => {
+    try {
+        res.render('login', {
+            logged_in: req.session.logged_in 
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+  });
+
   module.exports = router;
